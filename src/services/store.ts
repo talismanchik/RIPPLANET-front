9b1a7b599cffaaff7aa48d74aec9a42b9d443b73
talismanchik <talismanchik.by@gmail.com>
2024-09-23 10:25:31 +0300

    test

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,5 @@
 export const profilesHub: ProfileType[] = [
-    {name: 'User Name', nameEng: 'UserName', age: '35', years: '2000-2035', country: 'USA', like: true},
+    {name: 'User Name', nameEng: 'UserName', age: '1', years: '2000-2035', country: 'USA', like: true},
     {name: 'User Name', nameEng: 'UserName', age: '35', years: '2000-2035', country: 'USA', like: false},
     {name: 'User Name', nameEng: 'UserName', age: '35', years: '2000-2035', country: 'USAdsfsdafdsgsafghafhaagasfh', like: false},
     {name: 'User Name', nameEng: 'UserName', age: '35', years: '2000-2035', country: 'USA', like: true},
